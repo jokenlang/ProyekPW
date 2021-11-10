c98bbb9c88aa0729246e24c765d0cd1d5e41fc1f
jokenlim <jonathankenzie84@gmail.com>
2021-11-11 00:30:38 +0700

    tampilan login
    kebanyakan error tadi gara" ga konsen smoga bisa kerja tampilan register sblm tralu malem :"

--- index.php
+++ index.php
@@ -34,19 +34,39 @@ if (isset($_POST['login'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
-    <h1>Login</h1>
-    <form action="" method="post">
-        <label for="">Username :</label><input type="text" name="username" id="">
-        <br>
-        <label for="">Password :</label><input type="password" name="password" id="">
-        <br>
-        <input type="submit" value="Login" name="login">
-        <br>
-        <button name="register" value="register">Register</button>
-    </form>
+    <div class="banner">
+        <div class="bangunan"></div>
+        <div class="container">
+            <div class="apalah">
+                <div class="atas">
+                    <!-- <div class="kiri"></div> -->
+                    <h1>Login</h1>
+                    <div class="kanan">
+                    </div>
+                </div>
+                <div class="cb"></div>
+                <div class="bawah">
+                    <form action="" method="post">
+                    <label for="">Username :</label>
+                    <br>
+                    <input type="text" name="username" id="" placeholder="username">
+                    <br><br>
+                    <label for="">Password :</label>
+                    <br><input type="password" name="password" id="" placeholder="password">
+                    <br>
+                    <div class="button">
+                        <!-- <input type="submit" value="Login" name="login" class="login"> -->
+                        <button value="Login" name="login" class="login"><span>Login</span></button>
+                        <button name="register" value="register" class="register"><span>Register </span></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </body>
 
