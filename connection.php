c0657038b6ad70f9790f5166195084f34aed0af7
jokenlim <jonathankenzie84@gmail.com>
2021-11-25 21:49:31 +0700

    deploy ulang

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 $host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'furniture_website';
+$user = 'tstingp_zie';
+$password = '2207kenzie';
+$database = 'tstingp_furniture_website';
 $port = '3306';
-$conn = new mysqli($host, $user, $password, $database);
+// $conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, 'root', '', 'furniture_website');
 if ($conn->connect_errno) {
     die($conn->connect_error);
 }
